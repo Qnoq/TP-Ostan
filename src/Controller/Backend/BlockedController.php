d09663fc5596f4ed2a9c372e4c96e20e216e43fa
Creeminal <luciebrochet358@gmail.com>
2019-09-10 19:32:48 +0200

    ajout tabs sur gestion bloquage

--- src/Controller/Backend/BlockedController.php
+++ src/Controller/Backend/BlockedController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller\Backend;
 
+use App\Repository\CommentRepository;
 use App\Repository\PostRepository;
+use App\Repository\RoleRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,11 +18,17 @@ class BlockedController extends AbstractController
     /**
      * @Route("blocked", name="blocked_index")
      */
-    public function index(PostRepository $postRepository)
+    public function index(PostRepository $postRepository, UserRepository $userRepository, CommentRepository $commentRepository, RoleRepository $roleRepository)
     {
+        $users = $userRepository->findAll();
+        $comments = $commentRepository->findAll();
         $posts = $postRepository->findAll();
+        $roles = $roleRepository->findAll();
         return $this->render('backend/blocked/index.html.twig', [
             'posts' => $posts,
+            'users' => $users,
+            'comments' => $comments,
+            'roles' => $roles
             
         ]);
     }
