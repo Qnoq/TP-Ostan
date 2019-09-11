df5e27fc052d24b0d7869ccd08f94741e5382a7d
Karine56 <karinecode56@gmail.com>
2019-09-11 22:47:07 +0200

    messagerie envoi + reception croisée : user to user OK

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -41,6 +41,7 @@ class MessageController extends AbstractController
 
             // attribuer au message l'auteur du nouveau message
             $message->setUser($user);
+
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($message);
             $entityManager -> flush();
@@ -58,7 +59,7 @@ class MessageController extends AbstractController
     /**
      * @Route("/message/{title}", name="showmessage")
      */
-    public function show($title,MessageRepository $messageRepository)
+    public function show($title,MessageRepository $messageRepository, Request $request)
     {
         $messages = $messageRepository->findByTitle($title);
         // foreach pour traiter la donnée array récupérée => le getTitle, ds le render, sur un message à chaque fois
@@ -66,11 +67,44 @@ class MessageController extends AbstractController
             $message;
         }
 
+        //créer une réponse
+        $mess = new Message();
+        $form = $this->createForm(MessageType::class, $mess);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // récupérer l'utilisateur actuel - fonction native 
+            $user =$this->getUser();
+
+            // attribuer au message l'auteur du nouveau message
+            $mess->setUser($user);
+            // renvoyer le meme titre, récupérer grace au $message contenu dans le foreach
+            $mess->setTitle($message->getTitle());
+            //sélectionner le user receiver du message de réponse
+            //$receiver =  $message->getUserReceiver();
+            //dump($message->getUser());
+            //dump($receiver);die;
+
+            if($user == $message->getUser()){
+                // pour lui dire d'inverser le user et le receiver dans l'envoi du message : le user actuel renvoir à celui qui lui a envoyé le message
+                $mess->setUserReceiver($message->getUserReceiver());
+            }else if ($user == $message->getUserReceiver()){
+                $mess->setUserReceiver($message->getUser());
+            }
+            
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($mess);
+
+            //dump($mess->getUserReceiver());die;
+            $entityManager -> flush();
+            return $this->redirectToRoute('showmessage', ['title' => $mess->getTitle()]);
+        }
+
         return $this->render('message/showmessage.html.twig', [
             'title' => $message->getTitle(),
             'message' => $message,
             'messages' =>$messages,
-
+            'form' => $form->createView() 
         ]);
     }
 }
--- src/Form/MessageType.php
+++ src/Form/MessageType.php
@@ -15,26 +15,40 @@ class MessageType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder
-            ->add('content', TextareaType::class ,array(
-                'attr' => array(
-                    'placeholder' => 'Message..'
-                )
-            ))
-            //->add('createdAt')
-            //->add('email')
-            ->add('title', TextType::class, array(
-                'attr' =>array(
-                    'placeholder' => 'Titre du message'
-                )
-            ))
-            // pour définir, avec menu déroulant, le destinataire du message
-            ->add('userReceiver', EntityType::class,[
-                'class'=> User::class,
-                'expanded' =>false,
-                'multiple' =>false,
-            ])
-        ;
+        // récupérer l'url
+        $url = ($_SERVER["REQUEST_URI"]);
+
+        // construit un formulaire différent selon l'url, à partir de la meme Entité
+        if($url == '/message/new'){
+        
+            $builder
+                ->add('content', TextareaType::class ,array(
+                    'attr' => array(
+                        'placeholder' => 'Message..'
+                    )
+                ))
+                //->add('createdAt')
+                //->add('email')
+                ->add('title', TextType::class, array(
+                    'attr' =>array(
+                        'placeholder' => 'Titre du message'
+                    )
+                ))
+                // pour définir, avec menu déroulant, le destinataire du message
+                ->add('userReceiver', EntityType::class,[
+                    'class'=> User::class,
+                    'expanded' =>false,
+                    'multiple' =>false,
+                ])
+            ;
+        }else {
+            $builder
+                ->add('content', TextareaType::class ,array(
+                    'attr' => array(
+                        'placeholder' => 'Message..'
+                    )
+                ));
+        };
     }
 
     public function configureOptions(OptionsResolver $resolver)
