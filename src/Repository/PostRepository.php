8b2d97959ce083e8c5eb3966681671115d4c8f24
Karine56 <karinecode56@gmail.com>
2019-09-12 17:02:13 +0200

    debut query builder pour filter user receiver

--- src/Repository/PostRepository.php
+++ src/Repository/PostRepository.php
@@ -19,43 +19,7 @@ class PostRepository extends ServiceEntityRepository
         parent::__construct($registry, Post::class);
     }
 
-    // /**
-    //  * @return Post[] Returns an array of Post objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('p.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Ad
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
-    // public function findAllAdvicePost(){
-    //     return $this->getEntityManager()
-    //     ->createQuery('
-    //     SELECT p
-    //     FROM App\Entity\Post p
-    //     WHERE p.type = "article"
-    //     ')
-    //     ->getResult();
-    // }
+    
     public function findAllAdvicePost()
     {
         $query = $this->getEntityManager()->createQuery("
--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -37,32 +37,16 @@ class UserRepository extends ServiceEntityRepository
             ->getResult();
     }
 
-    // /**
-    //  * @return User[] Returns an array of User objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    public function findAllExceptUser()
     {
         return $this->createQueryBuilder('u')
-            ->andWhere('u.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('u.id', 'ASC')
-            ->setMaxResults(10)
+            ->andWhere('u.hidden = user')
+            ->setParameter('user', $user)
+            ->orderBy('m.name', 'ASC')
             ->getQuery()
             ->getResult()
         ;
     }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?User
-    {
-        return $this->createQueryBuilder('u')
-            ->andWhere('u.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
+
+
