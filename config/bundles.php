e220c0f6e729bd7dec9a76e41f7805220a28127b
Creeminal <luciebrochet358@gmail.com>
2019-09-19 13:34:31 +0200

    correctifs

--- config/bundles.php
+++ config/bundles.php
@@ -17,5 +17,4 @@ return [
     Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
-    FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
 ];
